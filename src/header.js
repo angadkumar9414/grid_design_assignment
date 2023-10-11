1727b5235fad4ebbfe8c32f60e78ed4ca3d33a2b
angadkumar9414 <angadkumar25788@gmail.com>
2023-10-11 11:58:04 +0530

    updating header and css

--- src/header.js
+++ src/header.js
@@ -1,27 +1,3 @@
-// function Header(){
-//     return(
-//         <div className="header">
-// <div className="wrap">
-//     <div className="logo">
-//         <div className="logo-written">EDYODA</div>
-//         <p className="para">Stories</p>
-//     </div>
-//     <div>
-//         <div className="drop-down">
-//             <p id="para">Explore Categories</p>
-//             <span className="material-symbols-outlined">
-//                 expand_more
-//             </span>
-//         </div>
-//     </div>
-// </div>
-// <div className="header-btn">
-//     <button>Go To Main Website</button>
-// </div>
-// </div>
-//  )
-// }
-
 const Header = () =>{
     return(
         <div id="header">
